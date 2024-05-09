2843554650e25c8c092d15e0edc1a6bc7b3c35c2
Single <1197826934@qq.com>
2024-05-09 17:23:21 +0800

    chore: vueUse

--- components/count.vue
+++ components/count.vue
@@ -6,4 +6,3 @@
 import { useCounterStore } from '@/stores/counter'
 const counterStore = useCounterStore()
 </script>
-
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@vueuse/nuxt'],
   css:['@/assets/css/tailwind.css'],
   postcss: {
     plugins: {
