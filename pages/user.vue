9ff7661f7b3e27e00584b4e245f3bbfe44b32ed3
Single <1197826934@qq.com>
2024-05-09 20:52:29 +0800

    test:测试git

--- pages/user.vue
+++ pages/user.vue
@@ -1,5 +1,23 @@
 <template>
     <div class="text-xl">用户</div>
     <count></count>
+    <div>VueUse:</div>
+    <div>当前鼠标位置:({{ x }}，{{ y }} )</div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useLocalStorage, useMouse, usePreferredDark } from '@vueuse/core'
+// tracks mouse position
+const { x, y } = useMouse()
+
+// if user prefers dark theme
+const isDark = usePreferredDark()
+
+// persist state in localStorage
+const store = useLocalStorage(
+    'my-storage',
+    {
+    name: 'Apple',
+    color: 'red',
+    },
+)
+</script>
