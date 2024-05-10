1b58d676eaf49c8a5ea379ce57104faf73cb8984
Single <1197826934@qq.com>
2024-05-10 09:42:37 +0800

    test:格式化

--- components/layoout/aside/item.vue
+++ components/layoout/aside/item.vue
@@ -1,27 +1,33 @@
 <template>
   <!-- 生效区域问题-> navigateTo 解决-->
-  <div class="flex group items-center pl-6 py-2 border-l-[4px] border-bg-base  hover:border-active-base cursor-pointer hover:text-active-base" @click="navigateTo(props.routeName)">
-      <div
-        class="bg-gray-base h-8 w-8 group-hover:bg-active-base  "
-        src="#"
-        mode="scaleToFill"
-      />
-      <div :to="props.routeName"  class="text-sm  w-full h-10 leading-10 text-gray-base group-hover:text-active-base pl-6">{{ props.content }}</div>
+  <div
+    class="flex group items-center pl-6 py-2 border-l-[4px] border-bg-base hover:border-active-base cursor-pointer hover:text-active-base"
+    @click="navigateTo(props.routeName)"
+  >
+    <div
+      class="bg-gray-base h-8 w-8 group-hover:bg-active-base"
+      src="#"
+      mode="scaleToFill"
+    />
+    <div
+      :to="props.routeName"
+      class="text-sm w-full h-10 leading-10 text-gray-base group-hover:text-active-base pl-6"
+    >
+      {{ props.content }}
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-const props = defineProps(
-  {
-    routeName: {
-      type: String,
-      required: true,
-      default: 'home'
-    },
-    content:{
-      type: String,
-      required: true,
-      default:'路由'
-    }
+const props = defineProps({
+  routeName: {
+    type: String,
+    required: true,
+    default: "home",
   },
-)
+  content: {
+    type: String,
+    required: true,
+    default: "路由",
+  },
+});
 </script>
