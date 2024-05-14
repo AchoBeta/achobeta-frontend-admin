c17c294f16f28c357d45086150067dde81d6c6b9
glows777 <1914426389@qq.com>
2024-05-14 15:58:59 +0800

    refactor: refactor code to use pinia in nuxt3

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,24 @@
 <script lang="ts" setup>
+import { useCounterStore } from '@/stores/index'
+
+const counterStore = useCounterStore()
+
+function handleAddCount() {
+  counterStore.increament()
+}
 </script>
 
 <template>
   <div class="mx-auto py-10">
     <div class="text-xl">
       首页
+      store{{ counterStore.count }}
+      <button @click="handleAddCount">
+        add count
+      </button>
+      <div class=" w-[28px] h-[28px] bg-slate-200">
+        <Icon-bot class=" text-2xl" />
+      </div>
     </div>
   </div>
 </template>
