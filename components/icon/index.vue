209e779814feeabfdc8fe7281a73599f4189a88d
glows777 <1914426389@qq.com>
2024-05-14 15:57:57 +0800

    feat:  add nuxt-svgo module to support using svg

---
+++ components/icon/index.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+export default {
+  props: {
+    filled: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    fontControlled: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    icon: {
+      type: Object,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: false,
+    },
+  },
+}
+</script>
+
+<template>
+  <component
+    :is="icon" :class="{
+      'nuxt-icon': fontControlled,
+      'nuxt-icon--fill': !filled,
+    }"
+  />
+</template>
+
+<style>
+.nuxt-icon {
+    width: 1em;
+    height: 1em;
+    margin-bottom: 0.125em;
+    vertical-align: middle;
+}
+
+.nuxt-icon--fill,
+.nuxt-icon--fill * {
+    fill: currentColor;
+}
+</style>
