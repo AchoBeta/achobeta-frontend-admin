f132f07cca7b597108afce99af7f0c18c00e44b3
Single <1197826934@qq.com>
2024-09-24 18:41:08 +0800

    perf：试题库

--- api/examPaper/types.ts
+++ api/examPaper/types.ts
@@ -15,7 +15,38 @@ export interface createPaper {
 }
 
 export interface EBankResData {
-  id: number
-  libType: string
-  createTime: string
+  list: List[];
+  /**
+   * 总页数
+   */
+  pages: number;
+  /**
+   * 总条数
+   */
+  total: number;
+  [property: string]: any;
+}
+
+export interface List {
+  /**
+   * 创建时间
+   */
+  createTime: string;
+  /**
+   * 试卷描述
+   */
+  description: string;
+  /**
+   * 试卷 id
+   */
+  id: number;
+  /**
+   * 试卷标题
+   */
+  title: string;
+  /**
+   * 更新时间
+   */
+  updateTime: string;
+  [property: string]: any;
 }
